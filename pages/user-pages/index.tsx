d918908c3771800505214cd05ad799a782316e3c
Arthur <arthur-fdmachado@educar.rs.gov.br>
2022-09-25 20:41:59 -0300

    getStaticProps with server path

--- pages/user-pages/index.tsx
+++ pages/user-pages/index.tsx
@@ -1,39 +1,39 @@
-function HomePage( /*{ data }: any*/ ) {
+function HomePage( { data }: any ) {
     /* Adicionar medições para cada um dos sensores/atuadores */
     return (
         <>
         <h1>Live Data:</h1>
 
         <div>
-            <h2> Temperatura: { /*data.temperature */ } </h2> 
+            <h2> Temperatura: { data.temperature } </h2> 
         </div>
 
         <div>
-            <h2> Humidade: { /*data.humidity*/ } </h2>
+            <h2> Humidade: { data.humidity } </h2>
         </div>
 
         <div>
-            <h2> Peltier Frio: { /*data.peltier_cold*/ } </h2>
+            <h2> Peltier Frio: { data.peltier_cold } </h2>
         </div>
 
         <div>
-            <h2> Peltier Quente: { /*data.peltier_warm*/ } </h2>
+            <h2> Peltier Quente: { data.peltier_warm } </h2>
         </div>
 
         <div>
-            <h2> Nível de água: { /*data.water_level*/ } </h2>
+            <h2> Nível de água: { data.water_level } </h2>
         </div>
         </>
     )
 } 
 
-// export async function getStaticProps() {
-//     const res = await fetch('http://localhost:3000/api/ESP-DataHandler');
-//     const data = await res.json();
+export async function getStaticProps() {
+    const res = await fetch('https://hydro-farm-app.vercel.app/ESP-DadaHandler');
+    const data = await res.json();
 
-//     console.log(data); 
+    console.log(data); 
 
-//     return { props: { data }, revalidate: 2 };
-// }
+    return { props: { data }, revalidate: 2 };
+}
 
 export default HomePage; 
