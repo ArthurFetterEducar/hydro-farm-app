0e3fe3fae1febf2a0aec60d7a1ca2c1c832f335d
Arthur <arthur-fdmachado@educar.rs.gov.br>
2022-10-29 14:29:38 -0300

    .

--- pages/api/ESP-DataHandler.js
+++ pages/api/ESP-DataHandler.js
@@ -33,24 +33,26 @@ async function ESP_API (req, res) {
         // });
 
         res.status(200).json(current_data);
-    } else if (req.method === "POST") {
-        current_data = req.body;
+    } else if (req.method === "POST") { 
+        current_data = req.body; 
         console.log(current_data); 
 
-        // const sensorData = await prisma.teste.create({
-        //     data: {
-        //         Temperature: 70,
-        //         Humidity: 100,          
-        //         Petier_Hot_Temperature: 22, 
-        //         Petier_Hot_State: true,
-        //         Peltier_Cold_Temperature: 11,
-        //         Peltier_Cold_State: false,
-        //         Water_Level: 0, 
-        //         Pump_State: false 
-        //     }
-        // });
+        
 
         res.status(201).send({message: 'Data Posted Successfully'});
+
+        const sensorData = await prisma.teste.create({
+            data: {
+                Temperature: 70,
+                Humidity: 100,          
+                Petier_Hot_Temperature: 22, 
+                Petier_Hot_State: true,
+                Peltier_Cold_Temperature: 11,
+                Peltier_Cold_State: false,
+                Water_Level: 0, 
+                Pump_State: false 
+            }
+        });
 
         // const sensorData = await prisma.teste.create({
         //     data: {
