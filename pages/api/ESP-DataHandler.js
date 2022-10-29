4d484ad19e9910a3968afe9872efe8e630b21d85
Arthur <arthur-fdmachado@educar.rs.gov.br>
2022-10-29 14:20:18 -0300

    .

--- pages/api/ESP-DataHandler.js
+++ pages/api/ESP-DataHandler.js
@@ -37,20 +37,33 @@ async function ESP_API (req, res) {
         current_data = req.body;
         console.log(current_data); 
 
-        res.status(201).send({message: 'Data Posted Successfully'});
-
         const sensorData = await prisma.teste.create({
             data: {
-                Temperature: current_data.Temperature,
-                Humidity: current_data.Humidity,          
-                Petier_Hot_Temperature: current_data.Petier_Hot_Temperature, 
-                Petier_Hot_State: current_data.Petier_Hot_State,
-                Peltier_Cold_Temperature: current_data.Peltier_Cold_Temperature,
-                Peltier_Cold_State: current_data.Peltier_Cold_State,
-                Water_Level: current_data.Water_Level, 
-                Pump_State: current_data.Pump_State 
+                Temperature: 70,
+                Humidity: 100,          
+                Petier_Hot_Temperature: 22, 
+                Petier_Hot_State: 1,
+                Peltier_Cold_Temperature: 11,
+                Peltier_Cold_State: 0,
+                Water_Level: 0, 
+                Pump_State: 1 
             }
         });
+
+        res.status(201).send({message: 'Data Posted Successfully'});
+
+        // const sensorData = await prisma.teste.create({
+        //     data: {
+        //         Temperature: current_data.Temperature,
+        //         Humidity: current_data.Humidity,          
+        //         Petier_Hot_Temperature: current_data.Petier_Hot_Temperature, 
+        //         Petier_Hot_State: current_data.Petier_Hot_State,
+        //         Peltier_Cold_Temperature: current_data.Peltier_Cold_Temperature,
+        //         Peltier_Cold_State: current_data.Peltier_Cold_State,
+        //         Water_Level: current_data.Water_Level, 
+        //         Pump_State: current_data.Pump_State 
+        //     }
+        // });
     }
 }
 
