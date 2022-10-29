fa4f697f8d0b5e662e7098d02d4b9f08c0a1278f
Arthur <arthur-fdmachado@educar.rs.gov.br>
2022-10-29 17:25:42 -0300

    .

--- pages/api/ESP-DataHandler.js
+++ pages/api/ESP-DataHandler.js
@@ -32,30 +32,19 @@ async function ESP_API (req, res) {
         console.log(req.body);
         console.log(current_data);
 
-        const sensorData = await prisma.teste2.create({
-            data: dummy_data
-        });
+        // const sensorData = await prisma.teste2.create({
+        //     data: dummy_data
+        // }); 
 
         res.status(200).json(current_data);
     } else if (req.method === "POST") { 
         current_data = req.body; 
         console.log(current_data); 
 
-        
-
-        res.status(201).send({message: "Data Posted Successfully"});
+        res.status(201).send({message: "Data Posted Successfully"}); 
 
         const sensorData = await prisma.teste2.create({
-            data: {
-                Temperature: 70,
-                Humidity: 100,          
-                Petier_Hot_Temperature: 22, 
-                Petier_Hot_State: true,
-                Peltier_Cold_Temperature: 11,
-                Peltier_Cold_State: false,
-                Water_Level: 0, 
-                Pump_State: false 
-            }
+            data: dummy_data
         });
 
         // const sensorData = await prisma.teste.create({
