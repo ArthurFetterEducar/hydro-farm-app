c9e5722a2b7229b5ecf486c37a9b30831fe56087
Arthur <arthur-fdmachado@educar.rs.gov.br>
2022-10-29 22:09:45 -0300

    .

--- pages/api/ESP-DataHandler.js
+++ pages/api/ESP-DataHandler.js
@@ -17,7 +17,7 @@ async function ESP_API (req, res) {
     if(req.method === "GET") {
         console.log("Got It");
 
-        res.status(200).json(current_data);
+        res.status(200).json(current_data.Temperature);
     } else if (req.method === "POST") { 
         current_data = req.body; 
 
